03665ba6ec58bb4b85aaabfa419ad3f9cd38b0e1
yangqun <qun.yang@non.se.com>
2021-07-05 12:34:23 +0800

    for表达式 模式匹配 scala中的异常捕捉

---
+++ src/main/scala/learning/day8/Test1.scala
@@ -0,0 +1,103 @@
+package learning.day8
+
+import java.time.DayOfWeek
+
+/**
+  * 模式匹配
+  * 注意scala中的模式匹配会进行自动break
+  * 1.根据值的类型和守卫进行匹配
+  * 2.scala期望模式变量名都以小写字母开头，而常量名则是大写字母
+  */
+class Test1 {}
+object Test1_1 {
+  def main(args: Array[String]): Unit = {
+    println(Week.SUNDAY)
+    println(Week.SUNDAY.toString)
+    val str = "SUNDAY"
+    println(str == Week.SUNDAY.toString)
+
+    Week.matchTest((1, 2))
+    Week.matchTest(3)
+
+  }
+}
+
+object Test2 {
+
+  def main(args: Array[String]): Unit = {
+    testMatchVariable(0)
+    testMatchVariable(100)
+  }
+  val max = 100
+  val MAX = 100
+
+  /**
+    * 如果你使用的是大写字母的名称，scala将会在作用域范围内查找常量，但是如果使用的是非大写字母的名称，它将只假设其是一个模式变量，
+    * 此处模式变量隐藏了（Sample类的）max字段
+    * @param input
+    */
+  def testMatchVariable(input: Int): Unit = {
+    input match {
+      case max => println(s"print max $max")
+      // 可以使用this来进行指定
+//      case this.max => println(s"print max $max")
+      // 可以使用单引号来进行指定
+//      case `max` => println(s"print max $max")
+    }
+    input match {
+      case MAX => println(s"print max $input")
+    }
+  }
+}
+
+object Week extends Enumeration {
+  val SUNDAY: Week.Value = Value("SUNDAY")
+  val SATURDAY: Week.Value = Value("SATURDAY")
+
+  /**
+    * 根据值的类型和守卫进行匹配
+    * @param input
+    */
+  def matchTest(input: Any): Unit = {
+    input match {
+      case (x: Int, y: Int)    => println("x & y")
+      case (x: Int) if (x > 1) => println("xxxx")
+    }
+  }
+}
+
+object Test3 {
+  def main(args: Array[String]): Unit = {
+    val age = 1
+    age match {
+      // 使用unapply提取器进行匹配，match表达式会自动的将age作为参数发送给apply方法
+      case Person() => println("+++++++++")
+      case _        => println("--------------")
+    }
+    val person = Person("yang", 123)
+    person match {
+      case Person(name, age) => println(person.name)
+      case _ => println("--------------")
+    }
+    val age1 = 11
+    age1 match {
+      // 使用unapply提取器进行匹配，match表达式会自动的将age作为参数发送给apply方法
+//      case Person() => println("+++++++++")
+        // 在这里number1和number不作为传入参数，而是作为传出参数
+//      case TestUnapply(number1,number2)        => println("--------------"+number1)
+      case TestUnapply(number1 @ Person(),number2)        => println("--------------"+number2)
+      case _ =>
+    }
+  }
+}
+object Person {
+  def unapply(age: Int) = {
+    if (age > 10) true else false
+  }
+}
+object TestUnapply {
+  def unapply(age: Int) = {
+    if (age == 10) Some(1, 2) else None
+  }
+}
+case class Person(name: String, age: Int)
---
+++ src/main/scala/learning/day8/Test3.scala
@@ -0,0 +1,44 @@
+package learning.day8
+
+/**
+ * 学习目标： scala异常
+ * 1.scala中不请之你对异常进行进行捕捉，因为它将所有的异常都看作是不受检异常，在java中需要区分受检异常和不受检异常，例如：
+ *   Thread.sleep方法，java中需要对该异常进行捕捉或者抛出来，但是Scala不需要
+ * 2.scala中catch代码块是结合着模式匹配来进行异常处理的
+ * 3.注意catch语句块的顺序，java的编译器会去检查catch代码块中的顺序，但是scala中不会去检查case中的代码块顺序，这个据说会在scala未来版本中进行改进
+ */
+case class Test3 ()
+
+object Test3_1{
+  def main(args: Array[String]): Unit = {
+    try{
+      numberException(11)
+    }catch{
+      case e: Exception => println("Exception"+e.getMessage)
+      case e: RuntimeException => println("RuntimeException"+e.getMessage)
+      case e: IllegalArgumentException => println("IllegalException"+"====="+e.getMessage)
+
+
+    }
+
+  }
+
+  def numberException(num:Int)={
+    if(num>10)
+      throw new IllegalArgumentException("illegalException")
+    if(num<10)
+      throw new RuntimeException("runTimeException")
+    if(num==10)
+      throw new Exception("exception")
+  }
+}
+
+/**
+ * java格式
+ */
+object Test3_2 {
+  @throws[InterruptedException]
+  def main(args: Array[String]): Unit = {
+    Thread.sleep(100)
+  }
+}

928d674d53302fd3c6e802706efec659d57ab3eb
yangqun <qun.yang@non.se.com>
2021-07-05 20:12:43 +0800

    scala并发学习

--- src/main/scala/learning/day10/Test3.scala
+++ src/main/scala/learning/day10/Test3.scala
@@ -12,14 +12,9 @@ class Test3 {
 
 object Test3_1{
   def main(args: Array[String]): Unit = {
-  val str = s"https://raw.githubusercontent.com/ReactivePlatform/" +
-    s"Pragmatic-Scala-StaticResources/master/src/main/resources/" +
-    s"weathers/"
-    println(str)
-  }
-
-  def getWeatherData(city:String)={
-//    val response = Source.fromURL()
+    timeSample { cities => cities map getWeatherData }
+    println("-----------------")
+    timeSample { cities ⇒ (cities.par map getWeatherData).toList }
   }
 
 
@@ -34,6 +29,23 @@ object Test3_1{
     val temperature = (xmlResponse \\ "temperature" \ "@value").text
     val condition = (xmlResponse \\ "weather" \ "@value").text
     (cityName, temperature, condition)
+  }
+
+  def printWeatherData(weatherData: (String, String, String)): Unit = {
+    val (cityName, temperature, condition) = weatherData
+    println(f"$cityName%-15s $temperature%-6s $condition")
+  }
+
+  def timeSample(getData: List[String] ⇒ List[(String, String, String)]): Unit = {
+    val cities = List("Houston,us", "Chicago,us", "Boston,us", "Minneapolis,us",
+      "Oslo,norway", "Tromso,norway", "Sydney,australia", "Berlin,germany",
+      "London,uk", "Krakow,poland", "Rome,italy", "Stockholm,sweden",
+      "Bangalore,india", "Brussels,belgium", "Reykjavik,iceland")
+
+    val start = System.nanoTime
+    getData(cities).sortBy(_._1).foreach(printWeatherData)
+    val end = System.nanoTime
+    println(s"Time taken: ${(end - start) / 1.0e9} sec")
   }
 }
 
